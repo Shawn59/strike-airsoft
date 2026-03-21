08c9c030cb9170edb2caeb329a807a0a3e2ab776
shawn59 <xxx_respekt.ru@mail.ru>
2026-03-21 22:55:59 +0500

    task_1 - реализован 3 блок

---
+++ src/views/MainPageView/ui/components/RulesBlock/RulesBlock.tsx
@@ -0,0 +1,30 @@
+import styles from './InfoBlock.module.scss';
+import { ContentLayout } from '@/widgets/Layouts/ui/ContentLayout';
+import { descriptionData } from '@/views/MainPageView/lib/InfoBlockData';
+import { SwiperImage } from '@/widgets/Swipers/ui/SwiperImage';
+import { carouselData } from '@/views/MainPageView/lib/InfoBlockData';
+import { DynamicModalImage } from '@/features/Modals/ui/ModalImage/DynamicModalImage';
+
+export const InfoBlock = () => {
+  return (
+    <div className={styles.infoBlock}>
+      <ContentLayout>
+        <div className={styles.blockContainer}>
+          <div className={styles.leftBlock}>{<SwiperImage data={carouselData} />}</div>
+
+          <div className={styles.rightBlock}>
+            <h1 className={styles.title}>{'О нас'}</h1>
+
+            <div className={styles.description}>
+              {descriptionData.map((value, index) => {
+                return <p key={index}>{value}</p>;
+              })}
+            </div>
+          </div>
+        </div>
+      </ContentLayout>
+
+      <DynamicModalImage />
+    </div>
+  );
+};
