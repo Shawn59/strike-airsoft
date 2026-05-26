e6ac41efa4dff652898f242b7ad9630fe67266d8
shawn59 <xxx_respekt.ru@mail.ru>
2026-05-26 22:47:05 +0500

    main - change price

--- src/views/CostPageView/lib/costData.ts
+++ src/views/CostPageView/lib/costData.ts
@@ -2,7 +2,7 @@ export const costData = {
   headers: ['Тип экипировки', 'Будний день', 'Выходные'],
   cells: [
     { name: 'Своя', price: 800, priceHol: 1000 },
-    { name: 'Стандартная', price: 1500, priceHol: 1800 },
-    { name: 'VIP', price: 2000, priceHol: 2300 },
+    { name: 'Стандартная', price: 1600, priceHol: 1900 },
+    { name: 'VIP', price: 2100, priceHol: 2400 },
   ],
 };
